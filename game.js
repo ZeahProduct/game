b24a48fedbbbf8d51c88f8fbeeb4a09e799e5bbc
ZeahProduct <werts1255@gmail.com>
2018-01-30 15:37:38 -0700

    Update game.js

--- game.js
+++ game.js
@@ -44,14 +44,14 @@ function start() {
 			var id = property.id;
 			document.getElementById("item").className = name;
 	    }
-	}.done(function() {
+	}).done(function() {
 		$.ajax({
 		    url: "https://cors-anywhere.herokuapp.com/http://services.runescape.com/m=itemdb_oldschool/viewitem?obj=" + id,
 		    success: function (data2) {
 			document.getElementById("item").src = $(data2).find(".item-description").find("img")[0].src;
 		    }
 		});
-	}));
+	});
 }
 
 $("#mainForm").submit(function(e) {
