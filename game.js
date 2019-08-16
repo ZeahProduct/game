2d10b104a06da32c5925bd3fe05efbb75bc6efb4
CJ <30376325+cjco6@users.noreply.github.com>
2019-08-16 03:04:00 -0700

    Better styling, levenshtein distance, loading gif

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ function load() {
 }
 
 function updatePointText() {
-	document.getElementById("points").innerHTML = "Points: " + points;
+	document.getElementById("points").innerHTML = '<img src="https://oldschool.runescape.wiki/images/6/63/Coins_detail.png?404bc" class="coins"> ' + points;
 }
 
 var randomProperty = function(obj) {
@@ -26,16 +26,40 @@ var randomProperty = function(obj) {
 	return obj[keys[keys.length * Math.random() << 0]];
 };
 
-function start() {
+function levenshtein(a, b) {
+	var t = [], u, i, j, m = a.length, n = b.length;
+	if (!m) {
+		return n;
+	}
+	if (!n) {
+		return m;
+	}
+	for (j = 0; j <= n; j++) {
+		t[j] = j;
+	}
+	for (i = 1; i <= m; i++) {
+		for (u = [i], j = 1; j <= n; j++) {
+			u[j] = a[i - 1] === b[j - 1] ? t[j - 1] : Math.min(t[j - 1], t[j], u[j - 1]) + 1;
+		}
+		t = u;
+	}
+	return u[n];
+}
+
+function start(firstTime) {
 	load();
-	var inputText = document.getElementById("itemName").value.toLowerCase();
-	var correctText = document.getElementById("item").className.replace(/%20/g, " ").toLowerCase();
-	document.getElementById("itemName").value = "";
-	if (inputText == correctText) {
-		console.log(correctText);
-		points += 1;
-		updatePointText();
-		save();
+	if (!firstTime) {
+		var inputText = document.getElementById("itemName").value.toLowerCase();
+		var correctText = document.getElementById("item").className.replace(/%20/g, " ").toLowerCase();
+		document.getElementById("itemName").value = "";
+		if (levenshtein(inputText, correctText) <= 4) {
+			points += 1;
+			updatePointText();
+			save();
+			document.getElementById("resultText").innerHTML = "Correct!";
+		} else {
+			document.getElementById("resultText").innerHTML = "Answer: " + correctText;
+		}
 	}
 	$.ajax({
 	    url: "https://cors-anywhere.herokuapp.com/http://rsbuddy.com/exchange/summary.json"
@@ -45,18 +69,21 @@ function start() {
 		var id = property.id;
 		document.getElementById("item").className = name;
 		$.ajax({
-		    url: "https://cors-anywhere.herokuapp.com/http://services.runescape.com/m=itemdb_oldschool/viewitem?obj=" + id,
-		    success: function (data2) {
-			document.getElementById("item").src = $(data2).find(".item-description").find("img")[0].src;
-		    }
+			url: "https://cors-anywhere.herokuapp.com/http://services.runescape.com/m=itemdb_oldschool/viewitem?obj=" + id,
+			success: function (data2) {
+				setTimeout(function() {
+					document.getElementById("item").src = $(data2).find(".item-description").find("img")[0].src;
+					document.getElementById("resultText").innerHTML = "What is this item?";
+				}, 2000);
+			}
 		});
 	});
 }
 
 window.onload = function() {
-	start();
+	start(true);
 	document.getElementById('mainForm').onsubmit = function(e) {
-	    e.preventDefault();
-	    start();
+		e.preventDefault();
+		start(false);
 	};
 }
